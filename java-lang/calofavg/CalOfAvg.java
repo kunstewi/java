61c24f5c301ed684421d464d84bc6db29736dc2c
kunstewi <kan.ai35423@gmail.com>
2025-05-29 14:14:49 +0530

    initial commit

---
+++ java-lang/calofavg/CalOfAvg.java
@@ -0,0 +1,12 @@
+package calofavg;
+
+public class CalOfAvg {
+    public static void main(String[] args) {
+        int x, y, z, avg;
+        x = 20;
+        y = 38;
+        z = 58;
+        avg = (x + y + z)/3;
+        System.out.println(avg);
+    }
+}
---
+++ java-lang/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class HelloWorld {
+    public static void main(String[] Args) {
+        System.out.println("Hello World from the helloworld package");
+    }
+}
